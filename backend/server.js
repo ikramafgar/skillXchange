f9eb9a9f755cb3543c4c9c452b4006f6d226867b
ikramafgar <ikramafgar@gmail.com>
2025-06-17 22:38:18 +0500

    add url

--- backend/server.js
+++ backend/server.js
@@ -103,7 +103,7 @@ app.use((err, req, res, next) => {
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "https://skillxchange-one.vercel.app/",
     credentials: true
   }
 });
